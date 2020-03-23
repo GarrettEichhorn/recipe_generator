e00c188f6f9e24c87bdcc8cabc556a8585b8561a
Garrett Eichhorn <garretteichhorn@Garretts-MacBook-Pro.local>
2020-03-23 12:45:05 -0500

    iitial commit

---
+++ static/js/data.js
@@ -0,0 +1,32 @@
+var data = [
+  {
+    id: 1,
+    datetime: "1/1/2010",
+    city: "benton",
+    state: "ar",
+    country: "us",
+    shape: "circle",
+    durationMinutes: "5 mins.",
+    comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
+  },
+  {
+    id: 2,
+    datetime: "1/1/2010",
+    city: "bonita",
+    state: "ca",
+    country: "us",
+    shape: "light",
+    durationMinutes: "13 minutes",
+    comments: "Three bright red lights witnessed floating stationary over San Diego New Years Day 2010"
+  },
+  {
+    id: 3,
+    datetime: "1/1/2010",
+    city: "el cajon",
+    state: "ca",
+    country: "us",
+    shape: "triangle",
+    durationMinutes: "6 minutes",
+    comments: "On New Years Eve I went outside to hear the celebration and fireworks in my neighbor hood. And noticed 3 red lights above my house and"
+  }
+  ];
